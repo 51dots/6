bd857ca89cb20f5188a0ecf4e694dce67fc04763
5dots <alarofalltrades@protonmail.com>
2023-08-07 20:55:10 -0400

    mint

---
+++ src/consts/parameters.ts
@@ -0,0 +1,22 @@
+/** Change these values to configure the application for your own use. **/
+
+// Your smart contract address (available on the thirdweb dashboard)
+// For existing collections: import your existing contracts on the dashboard: https://thirdweb.com/dashboard
+export const contractConst = "0x81AAB04105dD0a0e5131632981B1dAF21798bd2E";
+
+// The name of the chain your contract is deployed to.
+// Provide a string for standard chains, or import one from @thirdweb-dev/chains package.
+// Learn more: https://portal.thirdweb.com/react/react.thirdwebprovider#activechain-recommended
+export const chainConst = "polygon";
+
+// Configure the primary color for buttons and other UI elements
+export const primaryColorConst = "blue";
+
+// Choose between "light" and "dark" mode
+export const themeConst = "dark";
+
+// Gasless relayer configuration options
+export const relayerUrlConst = ""; // OpenZeppelin relayer URL
+export const biconomyApiKeyConst = ""; // Biconomy API key
+export const biconomyApiIdConst = ""; // Biconomy API ID
+export const clientIdConst = "92b1d7e44d4566da80570d102c0d32a8"; // Client ID
